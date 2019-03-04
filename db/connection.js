5819cc5b172fb69f6590aa78ee96792e14262dd3
Keith <ozmaneku@gmail.com>
2019-03-04 11:17:39 -0500

    Adjusted seed data

--- db/connection.js
+++ db/connection.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect("mongodb://localhost/recipe");
+  mongoose.connect("mongodb://localhost/Character_Data");
 }
 mongoose.connection.on("error", function(err) {
   console.error("MongoDB connection error: " + err);
