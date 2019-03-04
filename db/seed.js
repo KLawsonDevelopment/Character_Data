d4287e4737a654ac650b549926c7f8aec6e9b61e
Keith <ozmaneku@gmail.com>
2019-03-04 11:07:17 -0500

    Successfully created initial DB

--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,15 @@
 const mongoose = require('./connection.js')
 const Game = require('../models/Game')
 
-const Smash = new Game ({
+const smash = new Game ({
     name: "Super Smash Brothers Ultimate",
     year: 2018,
     company: "Nintendo",
     imgLink: "https://vignette.wikia.nocookie.net/nintendo/images/3/35/Super_Smash_Bros._Ultimate_logo.png/revision/latest/scale-to-width-down/640?cb=20180612174504&path-prefix=en"
 })
 
-Game.deleteMany({}).then(() =>
-Game.create(data)).then((game) =>
-console.log(game, "database seeded successfully")).catch((err) =>
-console.log(err))
+Game.deleteMany({})
+    .then(() => Game.create(smash))
+    .then(() => console.log(smash.name + " was added to datatbase"))
+    .catch((err) => console.log(err))
+    .then(() => mongoose.connection.close())
